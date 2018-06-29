baf344acbd2bed5c883d6160ed73a757ffa4d761
Eduardo Zimelewicz <eduardo.zimelewicz@gmail.com>
2018-06-29 11:05:18 -0300

    added time_fit

--- Project_3/README.md
+++ Project_3/README.md
@@ -1,9 +1,13 @@
 To run the project, you must run
 ```
-python ml_recommended.py
+python wine_recommend.py
 ```
 
 A recommendation system based by movie genre
 
-link for tutorial:
-https://www.kaggle.com/cclark/simple-content-based-recommendation-engine
+link for research sheets:
+https://docs.google.com/spreadsheets/d/1bUXbrqGn4J4RCs4D7H6ksvtWwO4cYVzFGRGrANfZKjs/edit?usp=sharing
+
+link for research annotations:
+https://docs.google.com/document/d/1ONnC8My3lqyl-WNhhpCkwV4wmKrTnUVnMZhFnn_q_VE/edit?usp=sharing
+
--- Project_3/wine_ml.py
+++ Project_3/wine_ml.py
@@ -3,7 +3,8 @@ from sklearn import datasets
 from sklearn import svm
 from sklearn import tree
 from sklearn import naive_bayes
-import time 
+import time
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split as tts
 
 wine = datasets.load_wine()
@@ -25,24 +26,35 @@ algoritmo3 = naive_bayes.GaussianNB()
 t_1 = time.time()
 algoritmo1.fit(train_feats, train_labels)
 t_2 = time.time()
-t_algo_1 = t_2 - t_1
+t_algo_fit_1 = t_2 - t_1
 
 t_1 = time.time()
 algoritmo2.fit(train_feats, train_labels)
 t_2 = time.time()
-t_algo_2 = t_2 - t_1
+t_algo_fit_2 = t_2 - t_1
 
 t_1 = time.time()
 algoritmo3.fit(train_feats, train_labels)
 t_2 = time.time()
-t_algo_3 = t_2 - t_1
+t_algo_fit_3 = t_2 - t_1
 
+t_1 = time.time()
 predictions1 = algoritmo1.predict(test_feats)
+t_2 = time.time()
 print "\nPredictions:", predictions1
+t_algo_1 = t_2 - t_1
+
+t_1 = time.time()
 predictions2 = algoritmo2.predict(test_feats)
+t_2 = time.time()
 print "\nPredictions:", predictions2
+t_algo_2 = t_2 - t_1
+
+t_1 = time.time()
 predictions3 = algoritmo3.predict(test_feats)
+t_2 = time.time()
 print "\nPredictions:", predictions3
+t_algo_3 = t_2 - t_1
 
 print "\n"
 
@@ -51,24 +63,30 @@ for i in range(len(predictions1)):
     if predictions1[i] == test_labels[i]:
         score += 1
 
+accu1 = (score / len(predictions1))
 print "SVM with linear kernel\n"
-print "Accuracy:", (score / len(predictions1)) * 100, "% \n"
+print "Fit_time:", t_algo_fit_1, "\n"
 print "Time:", t_algo_1, "\n"
+print "Accuracy:", accu1, "\n"
 
 score = 0
 for i in range(len(predictions2)):
     if predictions2[i] == test_labels[i]:
         score += 1
 
+accu2 = (score / len(predictions1))
 print "Decision Tree Classifier\n"
-print "Accuracy:", (score / len(predictions2)) * 100, "% \n"
+print "Fit_time:", t_algo_fit_2, "\n"
 print "Time:", t_algo_2, "\n"
+print "Accuracy:", accu2, "\n"
 
 score = 0
 for i in range(len(predictions3)):
     if predictions3[i] == test_labels[i]:
         score += 1
 
+accu3 = (score / len(predictions1))
 print "Naive Bayes\n"
-print "Accuracy:", (score / len(predictions3)) * 100, "% \n"
+print "Fit_time:", t_algo_fit_3, "\n"
 print "Time:", t_algo_3, "\n"
+print "Accuracy:", accu3, "\n"
